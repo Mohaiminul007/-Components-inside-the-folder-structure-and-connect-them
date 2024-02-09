8467024292886c77f0b2e80a2f7889cdd8c1d64c
Mohaiminul007 <mohaimin.durjoy@gmail.com>
2024-02-09 15:13:31 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-// import logo from './logo.svg';
+
+
 import './App.css';
-import Countries from './components/Countires/Countries';
-import Header from './components/Header/Header';
+import Countries from './components/Countries/Countries';
 
-
-import Person from './components/Person/Person';
-
-// import { useEffect, useState } from 'react';
 
 function App() {
+  
+
   return (
     <div className="App">
       
-      <Header></Header>
-      <Countries></Countries>
-      <Person></Person>
-      
+    <Countries></Countries>
        
-
       
     </div>
   );
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Noob = () => {
+const Country = () => {
     return (
         <div>
-            <h1>extension off</h1>
+            
         </div>
     );
 };
 
-export default Noob;
+export default Country;
